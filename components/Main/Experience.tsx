fab666712ec05bfd6e5f5ad9ee9c0de5d3c34b43
Niroyan Kugaruban <k.niroyan@gmail.com>
2023-12-16 17:56:50 +0530

    fix experience resposnive

--- components/Main/Experience.tsx
+++ components/Main/Experience.tsx
@@ -32,7 +32,7 @@ const Experience = () => {
           </span>
         </div>
 
-        <div className="w-full h-[80vh] grid grid-cols-3 grid-rows-2 gap-5 pt-[2rem] group">
+        <div className="w-full h-[80vh] grid grid-cols-3 grid-rows-2 gap-5 pt-[2rem] group p-3 lg:p-0">
           {Data.offers.map((offer) => (
             <div
               key={offer.id}
@@ -46,20 +46,20 @@ const Experience = () => {
                 width={1000}
                 height={1000}
               />
-              <h3 className="text-xl font-bold text-white z-50 text-center font-mono absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full flex items-center justify-center">
+              <h3 className="lg:text-xl font-bold text-white z-50 text-center font-mono absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full flex items-center justify-center">
                 {offer.title}
               </h3>
               <div
                 className={`flex w-full h-full z-50 items-center justify-center flex-col group-hover:backdrop-blur-sm top-0 left-0 right-0 bottom-0 absolute rounded-lg transition-all ${
                   hoveredOffer === offer.id ? "visible" : "hidden"
                 }`}>
-                <h3 className="text-xl font-bold text-white z-50 text-center font-mono">
+                <h3 className="lg:text-xl font-bold text-white z-50 text-center font-mono">
                   {offer.title}
                 </h3>
-                <h4 className="text-gray-200 z-50 font-serif">
+                <h4 className="text-gray-200 z-50 font-serif text-center">
                   {offer.subtitle}
                 </h4>
-                <p className="z-50 text-center font-sans text-gray-50 p-3">
+                <p className="z-50 text-center font-sans text-gray-50 p-3 hidden lg:visible">
                   {offer.description}
                 </p>
               </div>
